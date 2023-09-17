37d805e48e7ecf75b656a1bfb5549fad26670595
yuvalzoref <yuval_zoref@hotmail.com>
2023-09-17 11:56:22 +0100

    started building navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,31 +1,18 @@
 import { Link } from 'react-router-dom';
-import '../components/styling/navbar.scss'
+import '../components/styling/navbar.scss';
+import Logo from '../assets/logo.png';
 
 
 export default function Navbar() {
   return (
-    <div className="navbar">
-      <h1>This will be my Navbar</h1>
-          <button>
-          <Link to='../About'>
-            Click here to see about!
-          </Link>
-        </button>
-        <button>
-          <Link to='../Blog'>
-            Click here to see blog!
-          </Link>
-        </button>
-        <button>
-          <Link to='../Contact'>
-            Click here to see contact!
-          </Link>
-        </button>
-        <button>
-          <Link to='../'>
-            Click here to see the sexy homepage!
-          </Link>
-        </button>
-    </div>
+      <nav className="navbar">
+        <img src={Logo} alt='logo' className='logo'/>
+        <div className='navbarDesktop'>
+          <Link className='navbarDesktopLink' to='../About'>About</Link>
+          <Link className='navbarDesktopLink' to='../Blog'>Blog</Link>
+          <Link className='navbarDesktopLink' to='../Contact'>Contact</Link>
+          <Link className='navbarDesktopLink' to='../'>Home</Link>
+        </div>
+    </nav>
   )
 }
