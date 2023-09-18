22ef542bed65829e29ffb5e2e6d2d242b3e17678
yuvalzoref <yuval_zoref@hotmail.com>
2023-09-18 20:26:05 +0100

    changed homepage again

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,13 @@
 import { Link } from 'react-router-dom';
 import '../pages/styling/home.scss'
-import Seren from '../assets/serendipity.png'
+import Seren from '../assets/serendipity2.png'
 
 export default function Home() {
   return (
     <div id='home'>
       <div className='homeItems'>
-        <img src={Seren} alt='' className='homeTitlePic' />
           <Link activeClass='active' to='About' className='homeBtnLink'>
-            Click here to enter
+          <img src={Seren} alt='' className='homeTitlePic' />
           </Link>
       </div>
     </div>
