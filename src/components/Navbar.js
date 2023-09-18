56251e5896a14da4acd23173962a7e50252a9c57
yuvalzoref <yuval_zoref@hotmail.com>
2023-09-18 14:36:13 +0100

    added burger menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,12 @@
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../components/styling/navbar.scss';
 import Logo from '../assets/seren-logo.jpg';
+import Burger from '../assets/burger.png';
 
 
 export default function Navbar() {
+  const [showMenu, setShowMenu] = useState(false);
   return (
       <nav id="navbar">
         <img src={Logo} alt='logo' className='logo'/>
@@ -12,6 +15,14 @@ export default function Navbar() {
           <Link className='navbarDesktopLink' to='../Blog'>Blog</Link>
           <Link className='navbarDesktopLink' to='../Contact'>Contact</Link>
           <Link className='navbarDesktopLink' to='../'>Home</Link>
+        </div>
+
+        <img src={Burger} alt='burgerMenu' className='burgerMenu' onClick={()=>setShowMenu(!showMenu)}/>
+        <div className='navMenu' style={{display: showMenu? 'flex':'none'}}>
+          <Link className='navMenuLink' onClick={()=>setShowMenu(false)} to='../About'>About</Link>
+          <Link className='navMenuLink' onClick={()=>setShowMenu(false)} to='../Blog'>Blog</Link>
+          <Link className='navMenuLink' onClick={()=>setShowMenu(false)} to='../Contact'>Contact</Link>
+          <Link className='navMenuLink' onClick={()=>setShowMenu(false)} to='../'>Home</Link>
         </div>
     </nav>
   )
