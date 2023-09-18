b30907d50b30862fa43344b6a0d2f26807d450aa
yuvalzoref <yuval_zoref@hotmail.com>
2023-09-18 22:25:28 +0100

    changed some responsiveness

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../components/styling/navbar.scss';
-import Logo from '../assets/serendipity2.png';
+import Logo from '../assets/navbar/butterfly.png';
 import Burger from '../assets/navbar/burger.png';
 
 
