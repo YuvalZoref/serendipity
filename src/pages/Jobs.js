d371099212c731092c16ea2bdf8482b365354256
yuvalzoref <yuval_zoref@hotmail.com>
2024-08-21 12:34:01 +0100

    added job information with contact details

--- src/pages/Jobs.js
+++ src/pages/Jobs.js
@@ -8,9 +8,11 @@ export default function Jobs() {
       <Navbar />
         <div id='jobs'>
           <section className='jobsSection'>
-          <h1>JOB DESCRIPTION & PERSON SPECFICATION</h1>
+          <div className='jobTitle'>PROJECT MANAGER</div>
+          <br></br>
+          <div className='jobSubtitle'>Job description & person specification:</div>
             <br></br>
-            <h3>
+            <div className='jobDescription'>
             Serendipity Creative Writers is a Kirklees based UK Registered Charity with
             governance and accountability frameworks established by the Board of Trustees.
             We have a full time role for a period of 4 years to lead this small charity through
@@ -22,69 +24,81 @@ export default function Jobs() {
             the Charity management and administration. A skilled Project Manager
             to develop a business plan that will deliver the organisation's objectives.
             Someone who can contribute to the design of the Creative Writing Workshops to increase the scope and benefits.
+            </div>
+            <div className='jobMiddleSection'>
+              <br></br>
+              <ul className='dutiesList'>
+              <h3 className='dutiesListTitle'>Duties</h3>
+              <br></br>
+                <li>Work with the Trustees to develop a Business Development Plan</li><br></br>
+
+                <li>Work with the Trustees to ensure fulfilment of legal and financial responsibilities</li><br></br>
+
+                <li>Manage and oversee project timelines, resources, and budgets</li><br></br>
+
+                <li>Contribute to the composition of workshop content</li><br></br>
+
+                <li>Deliver the workshops to meet a planned schedule</li><br></br>
+
+                <li>Manage the team of volunteers</li><br></br>
+
+                <li>Progress the Fundraising Strategy to maximise Fundraising and Grant opportunities to ensure a long-term sustainable income</li><br></br>
+
+                <li>Develop creative writing business streams within Corporate organisations as a fundraising opportunity for the Charity</li><br></br>
+
+                <li>Ongoing reporting of delivery progress both within the Charity and to Funding Partners</li><br></br>
+
+                <li>Prepared to work flexible hours within a 37 hours’ framework</li><br></br>
+                <br></br>
+              </ul>
+              <ul className='qualiList'>
+              <h3 className='qualiListTitle'>Qualifications</h3>
+              <br></br>
+                <li>Creative Writing experience with a preference of having published work</li><br></br>
+
+                <li>Excellent presentation and communication skills to effectively liaise with team members and stakeholders and external Partners</li><br></br>
+
+                <li>Experience in UK Charity</li><br></br>
+
+                <li>Experience in project and product management, financial management</li><br></br>
+
+                <li>Ability to build strong relationships with potential funders and write major bids</li><br></br>
+
+                <li>Work in a creative and inspiring way that will engage others</li><br></br>
+
+                <li>Ability to think strategically</li><br></br>
+
+                <li>Educated to degree level minimum 2.2</li><br></br>
+              </ul>
+            </div>
+            <br></br>
+            <h3 className='jobBottomSection'>
+            If you are a proactive individual with a passion for Creative Writing, Business Development,
+            Project Management in a Charity environment and possess the necessary skills to drive successful project outcomes,
+            we encourage you to apply for this exciting opportunity.<br></br>
+            <br></br>
+            Application Information: Please submit a full CV with a covering letter explaining the fit between your skills and experience and this post.<br></br>
+
+            Closing date for applications: 09.09.2024<br></br>
+            <br></br>
+            Interviews:<br></br>
+
+            Hours of work: Full time – 37.5 hours per week with additional evening and weekend working as required for events. Flexible to meet business needs work from home and on site.<br></br>
+
+            Salary: £28,000 and subject to experience including expenses.<br></br>
+
+            Location: Kirklees, and wider has growth and expansion dictates.<br></br>
+
+            Reporting Structure: The PROJECT MANAGER reports to the Chair of the Board of Trustees.<br></br>
+            OWN VEHICLE REQUIRED (travel expenses provided) & clean licence
             </h3>
             <br></br>
-            <h4>
-            Duties
-
-              · work with the Trustees to develop a Business Development Plan
-
-              · work with the Trustees to ensure fulfilment of legal and financial responsibilities
-
-              · manage and oversee project timelines, resources, and budgets
-
-              · contribute to the composition of workshop content
-
-              · deliver the workshops to meet a planned schedule
-
-              · manage the team of volunteers
-
-              · progress the Fundraising Strategy to maximise Fundraising and Grant opportunities to ensure a long-term sustainable income
-
-              · develop creative writing business streams within Corporate organisations as a fundraising opportunity for the Charity
-
-              · ongoing reporting of delivery progress both within the Charity and to Funding Partners
-
-              · prepared to work flexible hours within a 37 hours’ framework.
-
-            Qualifications
-
-              · Creative Writing experience with a preference of having published work
-
-              · excellent presentation and communication skills to effectively liaise with team members and stakeholders and external Partners
-
-              · experience in UK Charity
-
-              · experience in project and product management, financial management
-
-              · ability to build strong relationships with potential funders and write major bids
-
-              · work in a creative and inspiring way that will engage others
-
-              · ability to think strategically
-
-              · educated to degree level minimum 2.2
-            </h4>
-            <br></br>
-            <h4>
-            If you are a proactive individual with a passion for Creative Writing, Business Development, Project Management in a Charity environment and possess the necessary skills to drive successful project outcomes, we encourage you to apply for this exciting opportunity.
-
-            Application Information: Please submit a full CV with a covering letter explaining the fit between your skills and experience and this post.
-
-            Closing date for applications: 09.09.2024
-
-            Interviews:
-
-            Hours of work: Full time – 37.5 hours per week with additional evening and weekend working as required for events. Flexible to meet business needs work from home and on site.
-
-            Salary: £28,000 and subject to experience including expenses.
-
-            Location: Kirklees, and wider has growth and expansion dictates.
-
-            Reporting Structure: The PROJECT MANAGER reports to the Chair of the Board of Trustees.
-
-            OWN VEHICLE REQUIRED (travel expenses provided) & clean licence
-            </h4>
+            <div className='jobsContact'>
+            <div className='jobsContactTop'>Please send C.V to:</div>
+            Mrs Kathy Trout<br></br>
+            kathy@westyorkshiredogrescue.co.uk<br></br>
+            07711617148
+            </div>
           </section>
         </div>
       <Footer />
